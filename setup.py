1b4e1161297e89107fcdcd93336bcb7f4ee048d8
Dias Alves <thomas.dias-alves@imag.fr>
2015-09-08 12:23:21 +0200

    setup with find packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='pyhapmap',
       version='0.1',
       description='Convert Hapmap data to numpy array / pandas dataframe',
       author='Thomas Dias-Alves',
-      packages=['pyhapmap'],
+      packages=find_packages(),
       test_suite='pyhapmap.tests',
       zip_safe=False)
